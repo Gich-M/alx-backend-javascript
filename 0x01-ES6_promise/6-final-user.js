ade98f5a2a84c7cd40b17305b783f144d0f85e65
Gich-M <gichmuriuki21@gmail.com>
2024-10-03 00:38:25 +0000

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser({
-      firstName: fileName,
+      firstName,
       lastName,
     }),
     uploadPhoto({
