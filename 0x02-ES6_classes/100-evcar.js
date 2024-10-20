8e26b163836179397b2ccefeacb8b8363efeb94d
Gich-M <gichmuriuki21@gmail.com>
2024-10-20 12:31:01 +0300

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -15,7 +15,7 @@ export default class EVCar extends Car {
   }
 
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
+    const Species = Object.getPrototypeOf(this).constructor[Symbol.species];
 
     return new Species();
   }
